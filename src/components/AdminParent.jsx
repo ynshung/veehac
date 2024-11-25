176f3184b24a2dd1457996f853aae2b3b5491bc3
Young Shung <me@ynshung.com>
2024-11-25 14:56:22 +0800

    redirect if invalid role

--- src/components/AdminParent.jsx
+++ src/components/AdminParent.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AdminDashboard from "./AdminDashboard";
 import AdminAssignment from "./AdminAssignment";
 
 function AdminParent() {
   const [showAssignment, setShowAssignment] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem("role") !== "admin") {
+      window.location.href = "/";
+    }
+  }, []);
+
   return (
     <div>
       {showAssignment !== '' ? (
