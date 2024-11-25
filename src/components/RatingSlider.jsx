4cfed63f58e98ec93c66b75e8ee6fb722369ed4c
Young Shung <me@ynshung.com>
2024-11-25 14:30:31 +0800

    store judging state in localstorage

--- src/components/RatingSlider.jsx
+++ src/components/RatingSlider.jsx
@@ -1,23 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/RatingSlider.css';
 
-const RatingSlider = ({ rating, handleChangeRating, tempResults, id, category }) => {
-  // console.log(projects)
-  // console.log(id)
-  // console.log(category)
-  // Initialize the state with the value of rating["rating"]
-  const [sliderValue, setSliderValue] = useState(rating["rating"]);
-
-  // Update the state whenever the rating["rating"] changes (e.g., if it's passed as a prop that may change)
-  useEffect(() => {
-    setSliderValue(rating);
-  }, [rating]);
-
-  // Handler for when the slider value changes
-  const handleSliderChange = (event) => {
-    setSliderValue(event.target.value);
-    handleChangeRating(category, parseInt(sliderValue) + 1)
-  };
+const RatingSlider = ({ rating, handleChangeRating, id, category }) => {
 
   return (
     <div className="slider-container">
@@ -29,8 +13,10 @@ const RatingSlider = ({ rating, handleChangeRating, tempResults, id, category })
         className="slider"
         id="slider"
         style={{ marginLeft: '4px' }}
-        value={sliderValue}
-        onChange={handleSliderChange} // Update the state when slider value changes
+        value={rating}
+        onChange={(e) =>{
+          handleChangeRating(e.target.value);
+        }}
       />
       <div className="labels">
         {Array.from({ length: 11 }, (_, i) => (
